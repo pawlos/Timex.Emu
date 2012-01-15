4b7c7df0d9009a1dfb058736d6326ffe3dfad962
Pawel Lukasik <lukasik.pawel+github@gmail.com>
2012-01-15 23:53:48 +0100

    First opcodes implemented Refactoring

--- python/cpu.py
+++ python/cpu.py
@@ -1,7 +1,15 @@
 from rom import ROM
 
+B = 0
+C = 1
+D = 2
+E = 3
+H = 4
+L = 5
+A = 7
+
 class CPU(object):
-	def __init__(self):
+	def __init__(self, isDebugged = False):
 		#Index registers
 		self.ix = 0x00;
 		self.iy = 0x00;
@@ -10,22 +18,43 @@ class CPU(object):
 		#special registers
 		self.i = 0x00;
 		self.r = 0x00;
+		self.isDebugged = isDebugged
 		
-		self.regs = []; #A,B,C,D,E,H,L
-		self.regsPri = []; #A',B',C',D',E',H',L;
-		self.fls = 0x00;
-		self.flsPri = 0x00; #flags'
+		self.iff = 0x00;
+		
+		self.regs = [0x00,0x00,0x00,0x00,0x00,0x00,0xFF,0x00]; #B,C,D,E,H,L,none,A
+		self.regsPri = [0x00,0x00,0x00,0x00,0x00,0x00,0xFF,0x00]; #B',C',D',E',H',L',none,A'
+		self.fls = [0x00];
+		self.flsPri = [0x00]; #flags'
 		
 		self.rom = ROM();
 		
-		self.dispatch = {			
-		};
+		self.dispatch = {
+			0xF3 : self.disableInterrupts,
+			0xAF : self.xorA
+		}
 	
 	def readOp(self):
-		opcode = self.rom.readMemory(self.pc);
-		print("0x%0.2x" % opcode)
-		self.pc+=1;
+		opcode = self.rom.readMemory(self.pc)		
+		disp = self.dispatch[opcode](opcode)
+		self.pc+=1
 
 	def run(self):
 		while True:
-			self.readOp();
+			self.readOp()
+	
+	def disableInterrupts(self, opcode):
+		"""DI"""
+		self.iff = 0x00;		
+		self.debug("DI")
+
+	def xorA(self, opcode):
+		"""XOR A"""
+		regInd = opcode & 7
+		self.regs[A] = self.regs[A] ^ self.regs[regInd]
+		self.debug("XOR A")
+
+	def debug(self, str):
+		"""print debug info"""
+		if (self.isDebugged):
+			print(str)
