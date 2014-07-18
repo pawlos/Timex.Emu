9a5f848a59d3290c0807a2c2ba0da934290dab5b
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2014-07-18 20:40:23 +0200

    Xor A test

--- python/tests_cpu.py
+++ python/tests_cpu.py
@@ -5,13 +5,20 @@ from cpu import CPU
 class TestCPUFunctions(unittest.TestCase):
 	def test_init_zeros_registers(self):
 		cpu = CPU(None)
-		self.assertEqual(0, cpu.registerA())
-		self.assertEqual(0, cpu.registerB())
+		self.assertEqual(0, cpu.A)
+		self.assertEqual(0, cpu.B)
 		self.assertEqual(0, cpu.registerC())
 		self.assertEqual(0, cpu.registerD())
 		self.assertEqual(0, cpu.registerE())
 		self.assertEqual(0, cpu.registerH())
 		self.assertEqual(0, cpu.registerL())
+
+	def test_xor_A_works_correctly(self):
+		cpu = CPU(None)
+		cpu.A = 12
+		cpu.B = 11
+		cpu.xorA(0)
+		self.assertEqual(7, cpu.A)
 
 
 if __name__ == '__main__':
