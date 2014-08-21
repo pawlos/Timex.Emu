088f81fd825feae072d9a8310d9e8a3ce0415b99
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2014-08-21 20:42:32 +0200

    Added tests for flags in LD (nn),rr

--- tests/tests_ld_nn_rr.py
+++ tests/tests_ld_nn_rr.py
@@ -13,3 +13,17 @@ class ld_nn_rr(unittest.TestCase):
 		cpu.readOp()
 		self.assertEqual(cpu.ram.readAddr(0x1000), 0x46)
 		self.assertEqual(cpu.ram.readAddr(0x1001), 0x44)
+
+	def test_ed43nn_does_not_affect_flags(self):
+		cpu = CPU(FakeRom('\xed\x43\x10\x00'))
+		cpu.CFlag = True
+		cpu.ZFlag = False
+		cpu.PVFlag = None
+		cpu.SFlag = True
+		cpu.HFlag = False
+		cpu.readOp()
+		self.assertTrue(cpu.CFlag)
+		self.assertFalse(cpu.ZFlag)
+		self.assertEqual(None, cpu.PVFlag)
+		self.assertTrue(cpu.SFlag)
+		self.assertFalse(cpu.HFlag)
