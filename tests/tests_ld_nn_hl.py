f130cd84748b7498f4a035d2f2968f0fd3e121d9
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2014-08-25 20:38:37 +0200

    LD (nn),HL flags tests

--- tests/tests_ld_nn_hl.py
+++ tests/tests_ld_nn_hl.py
@@ -13,3 +13,15 @@ class ld_nn_hl(unittest.TestCase):
 		cpu.readOp()
 		self.assertEqual(0x48, cpu.ram.readAddr(0xb22a))
 		self.assertEqual(0x3a, cpu.ram.readAddr(0xb229))
+
+	def test_ld_nn_hl_does_not_affect_flags(self):
+		cpu = CPU(FakeRom('\x22\xb2\x29'))
+		cpu.HFlag = False
+		cpu.ZFlag = True
+		cpu.PVFlag = False
+		cpu.SFlag = True
+
+		self.assertFalse(cpu.HFlag)
+		self.assertTrue(cpu.ZFlag)
+		self.assertFalse(cpu.PVFlag)
+		self.assertTrue(cpu.SFlag)
