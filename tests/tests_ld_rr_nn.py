f0098d119ebf0d1c234019b6d9ea5817bd6a8100
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2014-09-05 21:11:43 +0200

    LD HL, nn

--- tests/tests_ld_rr_nn.py
+++ tests/tests_ld_rr_nn.py
@@ -7,7 +7,12 @@ from loggers import Logger
 
 class ld_rr_nn(unittest.TestCase):
 
-	def test_ld_rr_nn_correctly_stores_value_to_Register(self):
+	def test_ld_BC_nn_correctly_stores_value_to_register(self):
 		cpu = CPU(FakeRom('\x01\xba\xab'))
 		cpu.readOp()
 		self.assertEqual(0xabba, cpu.BC)
+
+	def test_ld_hl_nn_correctly_stores_value_to_hl(self):
+		cpu = CPU(FakeRom('\x21\xfe\xca'))
+		cpu.readOp()
+		self.assertEqual(0xcafe, cpu.HL)
