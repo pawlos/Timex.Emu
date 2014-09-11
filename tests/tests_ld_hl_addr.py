4d63e25b193c08c14670291bb368a6f71e91019c
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2014-09-11 19:45:30 +0200

    LD HL, nn - flag tests

--- tests/tests_ld_hl_addr.py
+++ tests/tests_ld_hl_addr.py
@@ -13,3 +13,17 @@ class ld_hl_addr(unittest.TestCase):
 		cpu.ram.storeAddr(0x4546, 0xa1)
 		cpu.readOp()
 		self.assertEqual(0xa137, cpu.HL)
+
+	def test_ld_hl_does_not_affect_flags(self):
+		cpu = CPU(FakeRom('\x2a\x45\x45'))
+		cpu.ZFlag = True
+		cpu.PVFlag = False
+		cpu.HFlag = True
+		cpu.NFlag = False
+		cpu.SFlag = True
+		cpu.readOp()
+		self.assertTrue(cpu.ZFlag)
+		self.assertFalse(cpu.PVFlag)
+		self.assertTrue(cpu.HFlag)
+		self.assertFalse(cpu.NFlag)
+		self.assertTrue(cpu.SFlag)
