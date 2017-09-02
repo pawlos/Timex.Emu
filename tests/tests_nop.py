58953c1b4312bda7ad3803fd93dc2c175614e9fc
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-09-02 08:39:56 +0200

    Added few nop tests

---
+++ tests/tests_nop.py
@@ -0,0 +1,37 @@
+import unittest
+from cpu import CPU
+from opcodes import Opcodes
+from fakes import *
+from loggers import Logger
+
+class TestNOP(unittest.TestCase):
+
+	def test_nop_does_not_change_hl(self):
+		cpu = CPU(FakeRom('\x00'))
+		cpu.HL = 0x9999
+		cpu.readOp();
+		self.assertEqual(0x9999, cpu.HL)
+
+	def test_nop_does_not_change_de(self):
+		cpu = CPU(FakeRom('\x00'))
+		cpu.DE = 0x8999
+		cpu.readOp();
+		self.assertEqual(0x8999, cpu.DE)
+
+	def test_nop_does_not_change_bc(self):
+		cpu = CPU(FakeRom('\x00'))
+		cpu.BC = 0x1223
+		cpu.readOp()
+		self.assertEqual(0x1223, cpu.BC)
+
+	def test_nop_does_not_change_sp(self):
+		cpu = CPU(FakeRom('\x00'))
+		cpu.SP = 0x9799
+		cpu.readOp()
+		self.assertEqual(0x9799, cpu.SP)
+
+	def test_nop_does_not_change_a(self):
+		cpu = CPU(FakeRom('\x00'))
+		cpu.A = 0x99
+		cpu.readOp()
+		self.assertEqual(0x99, cpu.A)
