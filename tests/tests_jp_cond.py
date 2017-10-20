70617642a6e2ce5f5fad7305b85cdfe7dc95e149
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-10-20 08:19:13 +0200

    Test for JP cond

--- tests/tests_jp_cond.py
+++ tests/tests_jp_cond.py
@@ -7,8 +7,14 @@ from loggers import Logger
 
 class tests_jp_cond(unittest.TestCase):
 
-	def test_jp_c_jumps_if_ZFlag_is_non_zero(self):
+	def test_jp_c_jumps_if_ZFlag_is_set(self):
 		cpu = CPU(FakeRom('\xDA\x20\x15'))
 		cpu.CFlag = True
 		cpu.readOp()
 		self.assertEqual(0x1520, cpu.PC)
+
+	def test_jp_c_not_does_jump_if_ZFlag_is_not_set(self):
+		cpu = CPU(FakeRom('\xDA\x20\x15'))
+		cpu.CFlag = False
+		cpu.readOp()
+		self.assertEqual(0x03, cpu.PC)
