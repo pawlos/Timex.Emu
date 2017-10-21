aa0d7c5dc24e87deda95e730e8530d1ae6f0bf16
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-10-21 06:34:10 +0200

    JP conditional

--- tests/tests_jp_cond.py
+++ tests/tests_jp_cond.py
@@ -13,8 +13,20 @@ class tests_jp_cond(unittest.TestCase):
 		cpu.readOp()
 		self.assertEqual(0x1520, cpu.PC)
 
-	def test_jp_c_not_does_jump_if_CFlag_is_not_set(self):
-		cpu = CPU(FakeRom('\xDA\x20\x15'))
+	def test_jp_c_jumps_if_CFlag_is_not_set(self):
+		cpu = CPU(FakeRom('\xD2\x20\x15'))
 		cpu.CFlag = False
 		cpu.readOp()
-		self.assertEqual(0x03, cpu.PC)
+		self.assertEqual(0x1520, cpu.PC)
+
+	def test_jp_z_jumps_if_ZFlag_is_not_set(self):
+		cpu = CPU(FakeRom('\xC2\x20\x15'))
+		cpu.ZFlag = False
+		cpu.readOp()
+		self.assertEqual(0x1520, cpu.PC)
+
+	def test_jp_z_jumps_if_ZFlag_is_set(self):
+		cpu = CPU(FakeRom('\xCA\x20\x15'))
+		cpu.ZFlag = True
+		cpu.readOp()
+		self.assertEqual(0x1520, cpu.PC)
