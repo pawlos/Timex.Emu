5831352ae7bd45d6a281027e8865f493a67008b1
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-10-25 13:34:53 +0200

    call tests

--- tests/tests_call_cond.py
+++ tests/tests_call_cond.py
@@ -21,13 +21,20 @@ class tests_call_cond(unittest.TestCase):
 		self.assertEqual(0x1A, cpu.ram.readAddr(0x3001))
 		self.assertEqual(0x4A, cpu.ram.readAddr(0x3000))
 
-	'''def test_jp_z_jumps_if_ZFlag_is_set(self):
-		cpu = CPU(FakeRom('\xCA\x20\x15'))
-		cpu.ZFlag = True
+	def test_call_c_does_not_jumps_if_CFlag_is_set(self):
+		ram = FakeRam([None]*0x3002)
+		
+		rom = FakeRom('\x00'*0x1A47+'\xD4\x35\x21')
+		cpu = CPU(rom, ram)
+		cpu.PC = 0x1A47
+		cpu.SP = 0x3002
+		cpu.CFlag = True
 		cpu.readOp()
-		self.assertEqual(0x1520, cpu.PC)
 
-	def test_jp_po_jumps_if_PVFlag_is_not_set(self):
+		self.assertEqual(0x1A49, cpu.PC)
+		self.assertEqual(0x3002, cpu.SP)
+
+	'''def test_jp_po_jumps_if_PVFlag_is_not_set(self):
 		cpu = CPU(FakeRom('\xe0\x20\x15'))
 		cpu.PVFlag = False
 		cpu.readOp()
