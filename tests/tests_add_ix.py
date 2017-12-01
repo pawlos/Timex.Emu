b170dc27a8dc2e220f856db824a2dde33201684f
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-12-01 16:34:40 +0100

    ADD IX,BC tests

---
+++ tests/tests_add_ix.py
@@ -0,0 +1,14 @@
+import unittest
+from cpu import CPU
+from opcodes import Opcodes
+from fakes import *
+from loggers import Logger
+
+class tests_add_ix(unittest.TestCase):
+
+	def test_add_ix_bc_returns_correct_result(self):
+		cpu = CPU(FakeRom('\xdd\x09'))
+		cpu.IX = 0x1001
+		cpu.BC = 0x0bb0
+		cpu.readOp()
+		self.assertEqual(0x1bb1, cpu.IX)
