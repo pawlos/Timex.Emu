c54ef1d841abbbd747ab1b30c08ebf4657cba45e
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2017-12-03 16:33:37 +0100

    ADD IX,rr tests

--- tests/tests_add_ix.py
+++ tests/tests_add_ix.py
@@ -13,9 +13,22 @@ class tests_add_ix(unittest.TestCase):
 		cpu.readOp()
 		self.assertEqual(0x1bb1, cpu.IX)
 
-	def test_add_ix_bc_returns_correct_result(self):
+	def test_add_ix_de_returns_correct_result(self):
 		cpu = CPU(FakeRom('\xdd\x19'))
 		cpu.IX = 0x1001
 		cpu.DE = 0x0bb0
 		cpu.readOp()
 		self.assertEqual(0x1bb1, cpu.IX)
+
+	def test_add_ix_ix_returns_correct_result(self):
+		cpu = CPU(FakeRom('\xdd\x29'))
+		cpu.IX = 0x1001
+		cpu.readOp()
+		self.assertEqual(0x2002, cpu.IX)
+
+	def test_add_ix_sp_retursn_correct_result(self):
+		cpu = CPU(FakeRom('\xdd\x39'))
+		cpu.IX = 0x1001
+		cpu.SP = 0x0880
+		cpu.readOp()
+		self.assertEqual(0x1881, cpu.IX)
