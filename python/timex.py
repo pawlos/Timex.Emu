d5212959308a48f91ec99f1c3926a9b741514b09
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-01-06 19:36:59 +0100

    Fix typo

--- python/timex.py
+++ python/timex.py
@@ -7,8 +7,8 @@ from debugger import Debugger
 if __name__ == '__main__':
 	print("Starting execution...");
 	debugger = Debugger()
-	debugger.setBrekpoint(0x11CB)
-	debugger.setBrekpoint(0x11E2)
+	debugger.setBreakpoint(0x11CB)
+	debugger.setBreakpoint(0x11E2)
 	timex = CPU(debugger=debugger);
 	timex.run();
 	print("Ending...")
