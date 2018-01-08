e3fd0c582ea913998d43c7dc39a624354904d173
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-01-08 06:54:38 +0100

    RST opcode test

---
+++ tests/tests_rst.py
@@ -0,0 +1,15 @@
+import unittest
+from cpu import CPU
+from opcodes import Opcodes
+from fakes import *
+from loggers import Logger
+
+class tests_rst(unittest.TestCase):
+
+	def test_rst_does_sets_PC_correctly(self):
+		ram = FakeRam([None]*0x2002)
+		cpu = CPU(FakeRom('\x00'*0x15b3+'\xdf'), ram)
+		cpu.PC = 0x15B3
+		cpu.SP = 0x2000
+		cpu.readOp();
+		self.assertEqual(0x18, cpu.PC)
