65168d54fc3d446be604533c436316328794e988
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-01-09 20:29:09 +0100

    RST tests

--- tests/tests_rst.py
+++ tests/tests_rst.py
@@ -21,3 +21,51 @@ class tests_rst(unittest.TestCase):
 		cpu.SP = 0x2000
 		cpu.readOp();
 		self.assertEqual(0x0, cpu.PC)
+
+	def test_rst_8_does_sets_PC_correctly(self):
+		ram = FakeRam([None]*0x2002)
+		cpu = CPU(FakeRom('\x00'*0x15b3+'\xcf'), ram)
+		cpu.PC = 0x15B3
+		cpu.SP = 0x2000
+		cpu.readOp();
+		self.assertEqual(0x08, cpu.PC)
+
+	def test_rst_10_does_sets_PC_correctly(self):
+		ram = FakeRam([None]*0x2002)
+		cpu = CPU(FakeRom('\x00'*0x15b3+'\xd7'), ram)
+		cpu.PC = 0x15B3
+		cpu.SP = 0x2000
+		cpu.readOp();
+		self.assertEqual(0x10, cpu.PC)
+
+	def test_rst_20_does_sets_PC_correctly(self):
+		ram = FakeRam([None]*0x2002)
+		cpu = CPU(FakeRom('\x00'*0x15b3+'\xe7'), ram)
+		cpu.PC = 0x15B3
+		cpu.SP = 0x2000
+		cpu.readOp();
+		self.assertEqual(0x20, cpu.PC)
+
+	def test_rst_28_does_sets_PC_correctly(self):
+		ram = FakeRam([None]*0x2002)
+		cpu = CPU(FakeRom('\x00'*0x15b3+'\xef'), ram)
+		cpu.PC = 0x15B3
+		cpu.SP = 0x2000
+		cpu.readOp();
+		self.assertEqual(0x28, cpu.PC)
+
+	def test_rst_30_does_sets_PC_correctly(self):
+		ram = FakeRam([None]*0x2002)
+		cpu = CPU(FakeRom('\x00'*0x15b3+'\xf7'), ram)
+		cpu.PC = 0x15B3
+		cpu.SP = 0x2000
+		cpu.readOp();
+		self.assertEqual(0x30, cpu.PC)
+
+	def test_rst_38_does_sets_PC_correctly(self):
+		ram = FakeRam([None]*0x2002)
+		cpu = CPU(FakeRom('\x00'*0x15b3+'\xff'), ram)
+		cpu.PC = 0x15B3
+		cpu.SP = 0x2000
+		cpu.readOp();
+		self.assertEqual(0x30, cpu.PC)
