4aab5f3df3022d9b9dd3b4cfa63cba46587a3ad1
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-01-09 07:25:30 +0100

    RST 0 test

--- tests/tests_rst.py
+++ tests/tests_rst.py
@@ -13,3 +13,11 @@ class tests_rst(unittest.TestCase):
 		cpu.SP = 0x2000
 		cpu.readOp();
 		self.assertEqual(0x18, cpu.PC)
+
+	def test_rst_0_does_sets_PC_correctly(self):
+		ram = FakeRam([None]*0x2002)
+		cpu = CPU(FakeRom('\x00'*0x15b3+'\xc7'), ram)
+		cpu.PC = 0x15B3
+		cpu.SP = 0x2000
+		cpu.readOp();
+		self.assertEqual(0x0, cpu.PC)
