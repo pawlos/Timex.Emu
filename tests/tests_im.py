92cf814f5676736e50267eba28c9ac964cabe27f
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-01-21 12:53:06 +0100

    IM2 opcode test

--- tests/tests_im.py
+++ tests/tests_im.py
@@ -17,6 +17,11 @@ class tests_im(unittest.TestCase):
 		cpu.readOp()
 		self.assertEqual(0x00, cpu.interruptMode)
 
+	def test_im2_sets_interrupt_mode_to_2(self):
+		cpu = CPU(FakeRom('\xed\x5e'))
+		cpu.readOp()
+		self.assertEqual(0x02, cpu.interruptMode)
+
 	def test_im1_does_not_affect_flags(self):
 		cpu = CPU(FakeRom('\xed\x56'))
 		cpu.ZFlag = True
