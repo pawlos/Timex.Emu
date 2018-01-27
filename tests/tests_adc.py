2a7dcec397ee6fd73e3ee673b2a0a9d88ed53d48
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-01-27 07:31:35 +0100

    ADC HL, HL tests

--- tests/tests_adc.py
+++ tests/tests_adc.py
@@ -31,3 +31,10 @@ class tests_adc(unittest.TestCase):
 		cpu.CFlag = Bits.set()
 		cpu.readOp()
 		self.assertEqual(0XCDCD+0x1111+0x1,cpu.HL)
+
+	def test_add_HL_DE_with_C_flag_set_correctly_calculates_value(self):
+		cpu = CPU(FakeRom('\xed\x6a'))
+		cpu.HL = 0x1111
+		cpu.CFlag = Bits.set()
+		cpu.readOp()
+		self.assertEqual(0x1111+0x1111+0x1,cpu.HL)
