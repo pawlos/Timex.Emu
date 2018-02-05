3d28f82fb4e330a1e8fadab9c429da7a8a46a315
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-02-05 15:40:16 +0100

    Fix xor, or opcodes + tests

--- tests/tests_or_a.py
+++ tests/tests_or_a.py
@@ -25,3 +25,66 @@ class tests_or_a(unittest.TestCase):
 		cpu.A = 0xCD
 		cpu.readOp()
 		self.assertTrue(cpu.SFlag)
+
+	def test_or_a_e_perform_or_operation_value_is_correct(self):
+		cpu = CPU(FakeRom('\xb3'))
+		cpu.A = 0x01
+		cpu.E = 0x02
+		cpu.readOp();
+		self.assertEqual(0x03, cpu.A)
+
+	def test_or_a_b_perform_or_operation_value_is_correct(self):
+		cpu = CPU(FakeRom('\xb0'))
+		cpu.A = 0x01
+		cpu.B = 0x04
+		cpu.readOp();
+		self.assertEqual(0x05, cpu.A)
+
+	def test_or_a_c_perform_or_operation_value_is_correct(self):
+		cpu = CPU(FakeRom('\xb1'))
+		cpu.A = 0x01
+		cpu.C = 0x04
+		cpu.readOp();
+		self.assertEqual(0x05, cpu.A)
+
+	def test_or_a_d_perform_or_operation_value_is_correct(self):
+		cpu = CPU(FakeRom('\xb2'))
+		cpu.A = 0x01
+		cpu.D = 0x04
+		cpu.readOp();
+		self.assertEqual(0x05, cpu.A)
+
+	def test_or_a_e_perform_or_operation_value_is_correct(self):
+		cpu = CPU(FakeRom('\xb3'))
+		cpu.A = 0x01
+		cpu.E = 0x04
+		cpu.readOp();
+		self.assertEqual(0x05, cpu.A)
+
+	def test_or_a_h_perform_or_operation_value_is_correct(self):
+		cpu = CPU(FakeRom('\xb4'))
+		cpu.A = 0x01
+		cpu.H = 0x04
+		cpu.readOp();
+		self.assertEqual(0x05, cpu.A)
+
+
+	def test_or_a_l_perform_or_operation_value_is_correct(self):
+		cpu = CPU(FakeRom('\xb5'))
+		cpu.A = 0x01
+		cpu.L = 0x04
+		cpu.readOp();
+		self.assertEqual(0x05, cpu.A)
+
+
+	def test_or_a_a_perform_or_operation_value_is_correct(self):
+		cpu = CPU(FakeRom('\xb7'))
+		cpu.A = 0x00
+		cpu.B = 0x01
+		cpu.C = 0x02
+		cpu.D = 0x04
+		cpu.E = 0x08
+		cpu.H = 0x0a
+		cpu.L = 0x10
+		cpu.readOp();
+		self.assertEqual(0x00, cpu.A)
