ab2eb380dc1177d53809e7f8c43d91d3c3d7334e
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-03-27 09:45:38 +0200

    Fix typo

--- tests/tests_inc_hl.py
+++ tests/tests_inc_hl.py
@@ -4,9 +4,9 @@ from opcodes import Opcodes
 from fakes import *
 from loggers import Logger
 
-class tests_dec(unittest.TestCase):
+class tests_inc(unittest.TestCase):
 
-	def test_dec_hl_sets_correct_value_is_set(self):
+	def test_inc_hl_sets_correct_value_is_set(self):
 		ram = FakeRam([None]*0x200)
 		ram.storeAddr(0x100, 0xDD)
 		cpu = CPU(FakeRom('\x34'), ram)
