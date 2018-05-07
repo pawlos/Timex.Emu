c9d59b0f070d1469c33bd4ad7c347317c2bc2309
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-05-07 06:19:49 +0200

    Typo fix

--- tests/tests_ex.py
+++ tests/tests_ex.py
@@ -5,7 +5,7 @@ from fakes import *
 from loggers import Logger
 
 class tests_ex(unittest.TestCase):
-	def test_ex_de_hl_echanges_16_bit_hl_de_registers(self):
+	def test_ex_de_hl_exchanges_16_bit_hl_de_registers(self):
 		cpu = CPU(FakeRom('\xeb'))
 		cpu.HL = 0xabba
 		cpu.DE = 0xc0de
