c920483a34a5f4d919b4fcdc0334f60eff5cc580
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2018-10-27 23:31:41 +0200

    More pyflakes suggestions fixes

--- python/debugger.py
+++ python/debugger.py
@@ -1,9 +1,9 @@
 #Z80 debugger
 import sys
 import re
-from regs import *
+from regs import ZF, SF, HF, PVF, NF, CF, A, B, C, D, E, H, L
 from utility import Bits
-from loggers import *
+from loggers import EmptyLogger, Logger
 
 
 class EmptyDebugger(object):
@@ -40,97 +40,139 @@ class Debugger(object):
     def printBreakpoints(self):
         for pc in self.breakpoints:
             state = "active" if self.breakpoints[pc] else "inactive"
-            print "Breakpoint at {:04X}, {}".format(pc, state)
+            print("Breakpoint at {:04X}, {}"
+                  .format(pc, state))
 
     def attachDetachLogger(self, cpu):
         if type(cpu.logger) is not EmptyLogger:
-            print "Detaching logger"
+            print("Detaching logger")
             cpu.logger = EmptyLogger()
         else:
-            print "Attaching logger"
+            print("Attaching logger")
             cpu.logger = Logger(cpu)
 
     def help(self):
-        print "available commands"
-        print "ir - print info about 8-bit registers"
-        print "if - print info about flags"
-        print "ir16 - print info about 16-bit registers"
-        print "prom 0x<addr> - print value from ROM at <addr>"
-        print "pram 0x<addr> - print value from RAM at <addr>"
-        print "b 0x<addr> - set a breakpoint at <addr>"
-        print "bc 0x<addr> - clear a breakpoint at <addr>"
-        print "bd 0x<addr> - disable a breakpoint at <addr>"
-        print "bl - list all breakpoints"
-        print "log - attach/detach logger"
-        print "s - single step"
-        print "c - continue"
-        print "t - print timing info (m-cycles, t-states)"
-        print "? - this help"
-        print "exit - stop the program"
+        print("available commands")
+        print("ir - print info about 8-bit registers")
+        print("if - print info about flags")
+        print("ir16 - print info about 16-bit registers")
+        print("prom 0x<addr> - print value from ROM at <addr>")
+        print("pram 0x<addr> - print value from RAM at <addr>")
+        print("b 0x<addr> - set a breakpoint at <addr>")
+        print("bc 0x<addr> - clear a breakpoint at <addr>")
+        print("bd 0x<addr> - disable a breakpoint at <addr>")
+        print("bl - list all breakpoints")
+        print("log - attach/detach logger")
+        print("s - single step")
+        print("c - continue")
+        print("t - print timing info (m-cycles, t-states)")
+        print("? - this help")
+        print("exit - stop the program")
 
     def state(self, flag, flag_bit, flag_name):
         state = Bits.getNthBit(flag, flag_bit)
         return flag_name if state != 0 else flag_name.lower()
 
+    def print16bitregs(self, cpu):
+        print("BC : {:04X} "
+              "DE : {:04X} "
+              "HL : {:04X} "
+              "IX : {:04X} "
+              "IY : {:04X} "
+              "SP : {:04X}").format(
+            cpu.BC, cpu.DE, cpu.HL, cpu.IX, cpu.IY, cpu.SP)
+
+    def print8bitregs(self, cpu):
+        print("A : {:02X} "
+              "B : {:02X} "
+              "C : {:02X} "
+              "D : {:02X} "
+              "E : {:02X} "
+              "H : {:02X} "
+              "L : {:02X}"
+              .format(cpu.regs[A], cpu.regs[B], cpu.regs[C], cpu.regs[D],
+                      cpu.regs[E], cpu.regs[H], cpu.regs[L]))
+
+    def print8bitregsprim(self, cpu):
+        print("A': {:02X} "
+              "B': {:02X} "
+              "C': {:02X} "
+              "D': {:02X} "
+              "E': {:02X} "
+              "H': {:02X} "
+              "L': {:02X}"
+              .format(cpu.regsPri[A], cpu.regsPri[B], cpu.regsPri[C],
+                      cpu.regsPri[D], cpu.regsPri[E], cpu.regsPri[H],
+                      cpu.regsPri[L]))
+
+    def printflags(self, cpu):
+        print("{} {} _ "
+              "{} _ {} "
+              "{} {}"
+              .format(self.state(cpu.F, SF, "S"),
+                      self.state(cpu.F, ZF, "Z"),
+                      self.state(cpu.F, HF, "H"),
+                      self.state(cpu.F, PVF, "P/V"),
+                      self.state(cpu.F, NF, "N"),
+                      self.state(cpu.F, CF, "C")))
+
     def stop(self, cpu):
         while True:
-            input = raw_input("> ")
-            if input == "":
-                input = self.lastInput
+            cmd = raw_input("> ")
+            if cmd == "":
+                cmd = self.lastInput
 
-            self.lastInput = input
-            if "ir" == input:
-                print "A : {:02X} B : {:02X} C : {:02X} D : {:02X} E : {:02X} H : {:02X} L : {:02X}" \
-                    .format(cpu.regs[A], cpu.regs[B], cpu.regs[C],  cpu.regs[D],  cpu.regs[E], cpu.regs[H], cpu.regs[L])
-                print "A': {:02X} B': {:02X} C': {:02X} D': {:02X} E': {:02X} H': {:02X} L': {:02X}" \
-                    .format(cpu.regsPri[A], cpu.regsPri[B], cpu.regsPri[C], cpu.regsPri[D], cpu.regsPri[E], cpu.regsPri[H], cpu.regsPri[L])
-            elif "if" == input:
-                print "{} {} _ {} _ {} {} {}".format(self.state(cpu.F, SF, "S"), self.state(cpu.F, ZF, "Z"), \
-                                                     self.state(cpu.F, HF, "H"), self.state(cpu.F, PVF, "P/V"), \
-                                                     self.state(cpu.F, NF, "N"), self.state(cpu.F, CF, "C"))
-            elif "ir16" == input:
-                print "BC : {:04X} DE : {:04X} HL : {:04X} IX : {:04X} IY : {:04X} SP : {:04X}".format(cpu.BC, cpu.DE, cpu.HL, cpu.IX, cpu.IY, cpu.SP)
-            elif "prom " in input:
-                addr = self.getAddr(input)
-                print "Rom value at: 0x{:04X} is 0x{:02X}".format(addr, cpu.rom.readMemory(addr))
-            elif "pram " in input:
-                addr = self.getAddr(input)
-                print "RAM value at: 0x{:04X} is 0x{:02X}".format(addr, cpu.ram[addr])
-            elif "bl" == input:
-                print "List of breakpoints:"
+            self.lastInput = cmd
+            if "ir" == cmd:
+                self.print8bitregs(cpu)
+                self.print8bitregsprim(cpu)
+            elif "if" == cmd:
+                self.printflags(cpu)
+            elif "ir16" == cmd:
+                self.print16bitregs(cpu)
+            elif "pram " in cmd:
+                addr = self.getAddr(cmd)
+                print("RAM value at: 0x{:04X} is 0x{:02X}"
+                      .format(addr, cpu.ram[addr]))
+            elif "bl" == cmd:
+                print("List of breakpoints:")
                 self.printBreakpoints()
-            elif "bc " in input:
-                addr = self.getAddr(input)
+            elif "bc " in cmd:
+                addr = self.getAddr(cmd)
                 self.clearBreakpoint(addr)
-                print "Breakpoint cleared at: {:04X}".format(addr)
-            elif "bd " in input:
-                addr = self.getAddr(input)
+                print("Breakpoint cleared at: {:04X}".format(addr))
+            elif "bd " in cmd:
+                addr = self.getAddr(cmd)
                 self.disableBreakpoint(addr)
-                print "Breakpoint disabled at: {:04X}".format(addr)
-            elif "b " in input:
-                addr = self.getAddr(input)
+                print("Breakpoint disabled at: {:04X}".format(addr))
+            elif "b " in cmd:
+                addr = self.getAddr(cmd)
                 self.setBreakpoint(addr)
-                print "Breakpoint set at: {:04X}".format(addr)
-            elif "s" == input:
+                print("Breakpoint set at: {:04X}".format(addr))
+            elif "s" == cmd:
                 self.isSingleStepping = True
                 break
-            elif "c" == input:
+            elif "c" == cmd:
                 break
-            elif "t" == input:
-                print "m-cycles: {}, t-states: {}".format(cpu.m_cycles, cpu.t_states)
-            elif "log" == input:
+            elif "t" == cmd:
+                print("m-cycles: {}, t-states: {}".format(cpu.m_cycles,
+                                                          cpu.t_states))
+            elif "log" == cmd:
                 self.attachDetachLogger(cpu)
-            elif "?" == input:
-                print self.help()
-            elif "exit" == input:
+            elif "?" == cmd:
+                print(self.help())
+            elif "exit" == cmd:
                 sys.exit()
             else:
-                print "unknown command"
-                print self.help()
+                print("unknown command")
+                print(self.help())
+
+    def isBreakpoint(self, pc):
+        return pc in self.breakpoints and self.breakpoints[pc]
 
     def next_opcode(self, pc, cpu):
-        if (pc in self.breakpoints and self.breakpoints[pc]) or self.isSingleStepping:
+        if (self.isBreakpoint(pc)) or self.isSingleStepping:
             if self.isSingleStepping is False:
-                print "Stopped...@ 0x{:04X}".format(pc)
+                print("Stopped...@ 0x{:04X}".format(pc))
             self.isSingleStepping = False
             self.stop(cpu)
