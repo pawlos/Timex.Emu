1a79c5ccbf8427ddc77a32f603b54aca31ed122f
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2021-05-08 14:31:39 +0200

    Logger is now attached conditionally

--- python/timex.py
+++ python/timex.py
@@ -2,12 +2,18 @@
 from cpu import CPU
 from loggers import Logger
 from debugger import Debugger
+from sys import argv
 
 if __name__ == '__main__':
     print("Starting execution...")
+
     debugger = Debugger()
     debugger.setBreakpoint(0x16DD)
+
     timex = CPU(debugger=debugger)
-    timex.logger = Logger(timex)
+
+    if '--attach-logger' in argv:
+        timex.logger = Logger(timex)
+
     timex.run()
     print("Ending...")
