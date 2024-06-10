601b2327edd12d4d5d57505daf92c550da7549e8
Paweł Łukasik <lukasik.pawel@gmail.com>
2024-06-10 22:07:59 +0200

    Fix parameters parsing order

--- python/timex.py
+++ python/timex.py
@@ -16,24 +16,26 @@ if __name__ == '__main__':
     debugger.setBreakpoint(0x100)
 
     attach_logger = False
-    rom_loaded = False
+    rom_file = None
     mapAt = 0x0
     rom = ROM()
     for name, value in options:
         if name == '--mapAt':
             mapAt = int(value, 16)
-            rom = ROM(mapAt=mapAt)
             print(f'[+] Mapping ROM at 0x{mapAt:04X}.')
         if name == '--rom':
-            rom.loadFrom(value, False)
+            rom_file = value
             print(f'[+] ROM loaded from {value}.')
-            rom_loaded = True
         if name =='--attach-logger':
             attach_logger = True
             print(f'[+] Logger attached.')
 
-    if not rom_loaded:
+    if mapAt != 0x0:
+        rom = ROM(mapAt = mapAt)
+    if rom_file is None:
         rom.loadFrom('../rom/tc2048.rom')
+    else:
+        rom.loadFrom(rom_file, False)
 
     timex = CPU(debugger=debugger, rom=rom)
 
