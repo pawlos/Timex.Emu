38fafaa8c3c0518a905a90f8dd8e6b514a9563e0
Paweł Łukasik <lukasik.pawel@gmail.com>
2024-06-11 21:53:27 +0200

    Reorg params

--- python/timex.py
+++ python/timex.py
@@ -15,38 +15,37 @@ if __name__ == '__main__':
                                 "breakAt="])
     debugger = Debugger()
 
-    attach_logger = False
-    rom_file = None
-    mapAt = 0x0
-    break_at = None
+    params = {'debugger': False, 'rom_file': None, 'mapAt': 0x0, 'break_at': None}
     rom = ROM()
     for name, value in options:
         if name == '--mapAt':
             mapAt = int(value, 16)
+            params['mapAt'] = mapAt
             print(f'[+] Mapping ROM at 0x{mapAt:04X}.')
         if name == '--rom':
-            rom_file = value
+            params['rom_file'] = value
             print(f'[+] ROM loaded from {value}.')
         if name =='--attach-logger':
-            attach_logger = True
-            print(f'[+] Logger attached.')
+            params['debugger'] = True
+            print(f'[+] Debugger attached.')
         if name =="--breakAt":
             break_at = int(value, 16)
+            params['break_at'] = break_at
             print(f'[+] Setting breakpoint at 0x{break_at:04X}.')
 
-    if mapAt != 0x0:
-        rom = ROM(mapAt = mapAt)
-    if rom_file is None:
+    if params['mapAt'] != 0x0:
+        rom = ROM(mapAt = params['mapAt'])
+    if params['rom_file'] is None:
         rom.loadFrom('../rom/tc2048.rom')
     else:
-        rom.loadFrom(rom_file, False)
+        rom.loadFrom(params['rom_file'], False)
 
-    if break_at is not None:
-        debugger.setBreakpoint(break_at)
+    if params['break_at'] is not None:
+        debugger.setBreakpoint(params['break_at'])
 
     timex = CPU(debugger=debugger, rom=rom)
 
-    if attach_logger:
+    if params['debugger']:
         timex.logger = Logger(timex)
 
     print("Starting execution...")
